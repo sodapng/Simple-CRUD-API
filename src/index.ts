b312c48d638e3f973ce867a506f04184b19d0090
sodapng <sodapng@ya.ru>
2022-06-14 22:36:14 +0400

    feat: run server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello')
+import * as dotenv from 'dotenv'
+import { createServer } from 'http'
+import { resolve } from 'path'
+import { cwd } from 'process'
+
+dotenv.config({ path: resolve(cwd(), '.env') })
+
+const server = createServer((req, res) => {
+  res.end('Hello')
+})
+
+const port = process.env.PORT || 1337
+server.listen(port).on('listening', () => {
+  console.log(`HTTP is listening on ${port}`)
+})
